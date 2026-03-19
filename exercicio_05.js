a006f897d3603334d923b29e107b4447e1148d39
Pedro <pedro_vieira-juni@estudante.sesisenai.org.br>
2026-03-19 09:34:48 -0300

    add

---
+++ exercicio_05.js
@@ -0,0 +1,15 @@
+function ParImpar() {
+
+    let numero = [];
+
+    numero.push(Number(prompt("Digite um número: ")));
+
+    if (numero %  0) {
+        numero + 1;
+    }
+     else {
+        numero + 1;
+    }
+
+    
+}
