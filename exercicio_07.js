5641658dd2626f792f892ffebc443793c3cd54c4
Pedro <pedro_vieira-juni@estudante.sesisenai.org.br>
2026-03-24 08:28:45 -0300

    add - exercicio 6 e 7

---
+++ exercicio_07.js
@@ -0,0 +1,33 @@
+function Picoléteria() {
+
+    let codigo = prompt("Digite um codigo de um sorvete. 'a' até 'i'");
+
+if (codigo==a){
+    console.log ("Chocolate R$ 1,50");
+} 
+else if (codigo==b){
+    console.log ("Morango R$ 2,50");
+} 
+else if (codigo==c){
+    console.log ("Creme R$ 2,50");
+} 
+else if (codigo==d){
+    console.log ("Manga R$ 3,20");
+} 
+else if (codigo==e){
+    console.log ("Melancia R$ 3,40");
+} 
+else if (codigo==f){
+    console.log ("Vanilla Ice R$ 3,00");
+} 
+else if (codigo==g){
+    console.log ("Céu Azul R$ 3,60");
+} 
+else if (codigo==h){
+    console.log ("Brownie R$ 4,00");
+} 
+else if (codigo==i){
+    console.log ("Hawaiano R$ 5,00");
+} 
+
+}
